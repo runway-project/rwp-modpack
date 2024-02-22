0a385a94f4c2e30c5b7cbf3c76c3d10af3a5d3cb
Runway Project <rwp@hangar-api.space>
2024-02-21 19:57:26 -0500

    Finish up v1.2.0

--- scripts/build_modpack.ts
+++ scripts/build_modpack.ts
@@ -23,7 +23,7 @@ import {normalize, join} from 'path/posix'
 import archiver from 'archiver'
 
 /** Modpack version. Update this when content changes. */
-const VERSION = '1.1.0'
+const VERSION = '1.2.0'
 
 const include_file_extensions = ['png', 'dds', 'tga', 'jpg', 'jpeg', 'cfg', 'mu', 'dll', 'version', 'txt', 'md']
 
